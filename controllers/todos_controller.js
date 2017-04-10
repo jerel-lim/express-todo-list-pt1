ed92266bad7276d3b495f7e055c48c2b6aa14f66
Jerel Lim <j.limster@gmail.com>
2017-04-10 09:20:12 +0800

    updated create, but still cant work the updates and delete

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -44,68 +44,29 @@ router.delete('/:id', function (req, res) {
 })
 ///////////////////////////////////////////////////////////
 //
-// router.get('/:id', function (req, res) {
-//   Todo.findById(req.params.id, (err, todoItem) => {
-//     if (err) throw err
-//     res.render('single-todo', { todoItem: todoItem })
-//   })
-// })
-//
-// router.post('/showAndUpdateTodo', function (req, res) {
-//   res.redirect('/todos/' + req.body.id + '/edit')
-// })
-//
-// router.get('/todos/:id/edit', function (req, res) {
-//   Todo.findById(req.params.id, function (err, data) {
-//     if (err) {
-//       console.error(err)
-//       res.render('index')
-//       return
-//     }
-//     res.render('update', {updateTodo: data})
-//   })
-// })
-//
-// router.put('/todos', function (req, res) {
-//   console.log(req.body)
-//   Todo.findOneAndUpdate({ _id: req.body.id }, req.body, function (err, todo) {
-//     if (err) {
-//       console.error(err)
-//       res.render('index')
-//       return
-//     }
-//     // res.redirect('/todos/' + req.body.id)
-//     res.redirect('/todos')
-//   })
-// })
-
-
-// Todo.findOneAndUpdate({
-//   id: req.params._id
-// }, {
-//   title: req.body.title,
-//   description: req.body.description,
-//   completed: req.body.completed
-// }, (err, todoItem) => {
-//   if (err) throw err
-//   res.redirect('/todo/' + todoItem.id)
-// })
-
-
-
-function show (id) {
-  Todo.findById(id, function (err, todo) {
-    if (err) return console.log(err)
-    // console.log(todo)
+router.get('/:id', function (req, res) {
+  Todo.findById(req.params.id, (err, todoItem) => {
+    if (err) throw err
+    res.render('single-todo', { todoItem: todoItem })
   })
-}
+})
+//
 
-function update (id, params) {
-  Todo.findOneAndUpdate({ _id: id }, params, function (err, todo) {
-    if (err) console.log(err)
-    // console.log(todo)
-  })
-}
+
+//
+// function show (id) {
+//   Todo.findById(id, function (err, todo) {
+//     if (err) return console.log(err)
+//     // console.log(todo)
+//   })
+// }
+//
+// function update (id, params) {
+//   Todo.findOneAndUpdate({ _id: id }, params, function (err, todo) {
+//     if (err) console.log(err)
+//     // console.log(todo)
+//   })
+// }
 
 
 
